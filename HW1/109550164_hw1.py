a1402030eead7cacc8885bf21efb94d19716cee8
danzel-crazy <rickydanzel@gmail.com>
2022-10-26 00:25:20 +0800

    comment

--- HW1/109550164_hw1.py
+++ HW1/109550164_hw1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import math
 
+# out predict y into sigmoid function
 def your_model_sigmoid(train, w, b):
     y = np.ndarray(train.shape)
     y.fill(0)
@@ -10,6 +11,7 @@ def your_model_sigmoid(train, w, b):
         y[i] = (1/(1 + math.exp(-(train[i][0]*w[0][0] + b[0][0]))))
     return y
 
+# compute error through cross entropy between ground turth and predict y
 def Cross_entropy_error(test, pred, w, b):
     error = 0
     n = len(test)
@@ -17,6 +19,7 @@ def Cross_entropy_error(test, pred, w, b):
         error -= (test[i]*math.log(pred[i]) + (1-test[i])*math.log((1-pred[i])))
     return error
 
+#use logistic regression to compute w and b 
 def logistic_regression(w, b, x, y, theta):
     error = 0
     temp_y = np.ndarray(x.shape)
@@ -31,20 +34,22 @@ def logistic_regression(w, b, x, y, theta):
         gradient_w = 0 
         omega = your_model_sigmoid(x, new_w, new_b)
         for i in range(n):
+            #do the derivative
             gradient_w += (omega[i]-y[i])*x[i][0]
             gradient_b += (omega[i]-y[i]) 
         
         for i in range(n):
             new_w[i][0] -= theta*gradient_w
             new_b[i][0] -= theta*gradient_b
-        # temp_y = your_model(x, new_w, new_b)
+        
         error = Cross_entropy_error(y, omega, new_w, new_b)
-
+        #plot error result
         plt.scatter(j, error)
         plt.pause(0.05)
     
     return new_w, new_b
 
+#compute y = wx + b
 def your_model(train, w, b):
     y = np.ndarray(train.shape)
     y.fill(0)
@@ -52,6 +57,7 @@ def your_model(train, w, b):
         y[i] = (train[i][0]*w[0][0]+b[0][0])
     return y
 
+#compute mean square error between ground turth and predict y
 def Mean_square_error(test, pred, w, b):
     error = 0
     for i in range(len(test)):
@@ -59,6 +65,7 @@ def Mean_square_error(test, pred, w, b):
     error = error/len(test)
     return error/2
 
+#do linear regression to get w and b
 def linear_regression(w, b, x, y, theta):
     error = 0
     temp_y = np.ndarray(x.shape)
@@ -71,6 +78,7 @@ def linear_regression(w, b, x, y, theta):
         gradient_b = 0
         gradient_w = 0 
         for i in range(n):
+            #do the derivative
             gradient_b += -(2/n) * (y[i] - ((new_w[i][0] * x[i][0]) + new_b[i][0]))
             gradient_w += -(2/n) * x[i][0] * (y[i] - ((new_w[i][0] * x[i][0]) + new_b[i][0]))
             
@@ -79,14 +87,18 @@ def linear_regression(w, b, x, y, theta):
             new_b[i][0] -= theta * gradient_b
         temp_y = your_model(x, new_w, new_b)
         error = Mean_square_error(y, temp_y, new_w, new_b)
-
+        #plot error result
         plt.scatter(j, error)
         plt.pause(0.05)
     
     return new_w, new_b
 
 def main():
+    #linear regression main part
+
+    #data load
     x_train, x_test, y_train, y_test = np.load('C:/Users/danzel/Hsu/課程/大三上/機器學習/HW1/regression_data.npy', allow_pickle=True)
+    #initiate w and b
     init_b = np.ndarray(x_train.shape)
     init_b.fill(0)
     init_w = np.ndarray(x_train.shape)
@@ -99,7 +111,11 @@ def main():
     print('Mean_square_error:',Mean_square_error(y_test, y_pred, new_w, new_b))
     plt.show()
 
+    #logistic regression main part
+    
+    #data load
     x_train_1, x_test_1, y_train_1, y_test_1 = np.load('C:/Users/danzel/Hsu/課程/大三上/機器學習/HW1/classification_data.npy', allow_pickle=True)
+    #initiate w and b
     init_b = np.ndarray(x_train_1.shape)
     init_b.fill(0)
     init_w = np.ndarray(x_train_1.shape)
